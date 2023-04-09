886a15e83b161a0142dd5011f60b08c0f3015734
mairh <ujjwal.mairh@gmail.com>
2023-04-09 15:00:50 +0300

    Fix app creation race

--- src/modules/firebase/firebase.constants.ts
+++ src/modules/firebase/firebase.constants.ts
@@ -1,7 +1,5 @@
 export const FIREBASE_APP_IN_REQUEST_PATH = Symbol('AppInRequestPath');
 
-export const FIREBASE_USER_IN_REQUEST_PATH = Symbol('UserInRequestPath');
-
 export enum FirebaseDatabasePath {
   USERS = 'users',
   BUYER_PROFILES = 'buyerProfiles',
