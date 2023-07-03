504273ec076cdeb8b7cf84c575c54342bf10e734
Aleksi Pousar <aleksi1578@msn.com>
2023-07-03 15:21:53 +0300

    add stashed changes for subscription

--- src/modules/userMerge/userMerge.resolver.ts
+++ src/modules/userMerge/userMerge.resolver.ts
@@ -1,17 +1,22 @@
-import { UseFilters } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Inject, UseFilters, UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 
 import { FirebaseApp } from '../firebase/firebase.decorator';
 import { FirebaseAppInstance } from '../firebase/firebase.types';
 import { FirebaseExceptionFilter } from '../firebase/firebase.exception.filter';
 import { UserMergeDto } from './dto/userMerge/userMerge.dto';
 import { UserMergeService } from './userMerge.service';
-import { UserMergeDB } from 'hero24-types';
 import { UserMergeInput } from './dto/userMerge/userMerge.input';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { PubSub } from 'graphql-subscriptions';
+import { PUBSUB_PROVIDER } from '../graphql-pubsub/graphql-pubsub.constants';
 
 @Resolver()
 export class UserMergeResolver {
-  constructor(private UserMergeService: UserMergeService) {}
+  constructor(
+    private UserMergeService: UserMergeService,
+    @Inject(PUBSUB_PROVIDER) private pubSub: PubSub,
+  ) {}
 
   @Query(() => UserMergeDto, { nullable: true })
   @UseFilters(FirebaseExceptionFilter)
@@ -29,5 +34,11 @@ export class UserMergeResolver {
     @FirebaseApp() app: FirebaseAppInstance,
   ): Promise<UserMergeDto | null> {
     return await this.UserMergeService.startUserMerge(userMergeInput, app);
+  }
+
+  @Subscription(() => UserMergeDto)
+  @UseGuards(AuthGuard)
+  subscribeToUserMerge(@Args('userId') userId: string) {
+    return this.pubSub.asyncIterator('userMerge');
   }
 }
