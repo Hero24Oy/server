36c17857b5450861ea1d60a830ef9a727b1e7a4a
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2023-07-13 18:54:27 +0300

    Define all necessary deal properties

--- src/modules/hub-spot/hub-spot-deal/hub-spot-deal.types.ts
+++ src/modules/hub-spot/hub-spot-deal/hub-spot-deal.types.ts
@@ -8,4 +8,11 @@ export type HubSpotDealProperties = {
   [HubSpotDealProperty.AMOUNT]: string;
   [HubSpotDealProperty.SELLER_PROFILE]: string;
   [HubSpotDealProperty.BUYER_PROFILE]: string;
+  [HubSpotDealProperty.DURATION]: string;
+  [HubSpotDealProperty.PRICE_PER_HOUR]: string;
+  [HubSpotDealProperty.SERVICE_CATEGORY]: string;
+  [HubSpotDealProperty.DEAL_TYPE]: string;
+  [HubSpotDealProperty.DEAL_OWNER]: string;
+  [HubSpotDealProperty.DEAL_STAGE]: string;
+  [HubSpotDealProperty.CLOSE_DATE]: string;
 };
