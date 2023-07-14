2bbf9b5d2cec0871912f9c6211335b63d3a5bb06
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2023-07-14 15:40:53 +0300

    Add dealOwner to .env

--- src/config/hubSpot.ts
+++ src/config/hubSpot.ts
@@ -4,8 +4,12 @@ export const hubSpotValidationSchema = Joi.object({
   HUB_SPOT_ACCESS_TOKEN: Joi.string()
     .regex(/[^/]$/, "Shouldn't end with '/'")
     .required(),
+  HUB_SPOT_DEAL_OWNER: Joi.string()
+    .regex(/[^/]$/, "Shouldn't end with '/'")
+    .required(),
 });
 
 export default () => ({
   accessToken: process.env.HUB_SPOT_ACCESS_TOKEN,
+  dealOwner: process.env.HUB_SPOT_DEAL_OWNER,
 });
