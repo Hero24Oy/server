f0e679ccd6695f16986eb687e3427d683db05b1e
mairh <ujjwal.mairh@gmail.com>
2023-07-24 14:21:49 +0300

    Add stripe sdk package and create stripe module

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,6 +23,8 @@ import { SubscriberModule } from './modules/subscriber/subscriber.module';
 import { OfferModule } from './modules/offer/offer.module';
 import { PriceCalculatorModule } from './modules/price-calculator/price-calculator.module';
 import { UserMergeModule } from './modules/user-merge/user-merge.module';
+import { GraphQLConnectionParams } from './app.types';
+import { StripeModule } from './modules/stripe/stripe.module';
 
 @Module({
   imports: [
@@ -42,6 +44,10 @@ import { UserMergeModule } from './modules/user-merge/user-merge.module';
         autoSchemaFile: true,
         subscriptions: {
           'graphql-ws': true,
+          'subscriptions-transport-ws': {
+            onConnect: (connectionParams: GraphQLConnectionParams) =>
+              graphQLManagerService.createContext({ connectionParams }),
+          },
         },
         playground: configService.get<boolean>('app.isDevelopment'),
         context: async (ctx) => graphQLManagerService.createContext(ctx),
@@ -63,6 +69,7 @@ import { UserMergeModule } from './modules/user-merge/user-merge.module';
     OfferModule,
     PriceCalculatorModule,
     UserMergeModule,
+    StripeModule,
   ],
   providers: [AppResolver],
 })
