b8ac561d4eb2fa702c0050226b15b8fa50609037
Maksym Pryhoda <maksym.pryhoda@sysgears.com>
2023-08-02 17:05:29 +0300

    Add subscription to offer updates.

---
+++ src/modules/offer/offer.event-handlers.ts
@@ -0,0 +1,9 @@
+import { PubSub } from 'graphql-subscriptions';
+import { createSubscriptionEventEmitter } from '../graphql-pubsub/graphql-pubsub.utils';
+import { OFFER_UPDATED_SUBSCRIPTION } from './offer.constants';
+import { createOfferEventHandler } from './offer.utils';
+
+export const updateOfferEventHandler = (pubsub: PubSub) =>
+  createOfferEventHandler((offer) => {
+    createSubscriptionEventEmitter(OFFER_UPDATED_SUBSCRIPTION)(pubsub, offer);
+  });
