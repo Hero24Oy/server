f1668c55e798967af78b53a26c448eff14762702
Maksym Pryhoda <maksym.pryhoda@sysgears.com>
2023-08-08 12:24:49 +0300

    Remove subscription from OfferHubSpotModule.

--- src/modules/offer/offer-hub-spot/offer-hub-spot.module.ts
+++ src/modules/offer/offer-hub-spot/offer-hub-spot.module.ts
@@ -1,14 +1,11 @@
 import { Module, forwardRef } from '@nestjs/common';
 
-import { SubscriptionManagerModule } from 'src/modules/subscription-manager/subscription-manager.module';
 import { HubSpotDealModule } from 'src/modules/hub-spot/hub-spot-deal/hub-spot-deal.module';
 import { UserModule } from 'src/modules/user/user.module';
 import { OfferRequestModule } from 'src/modules/offer-request/offer-request.module';
-import { FirebaseModule } from 'src/modules/firebase/firebase.module';
 
 import { OfferPriceCalculatorModule } from '../offer-price-calculator/offer-price-calculator.module';
 import { OfferHubSpotService } from './offer-hub-spot.service';
-import { OfferHubSpotSubscription } from './offer-hub-spot.subscription';
 import { OfferModule } from '../offer.module';
 import { FeeModule } from 'src/modules/fee/fee.module';
 
@@ -22,10 +19,6 @@ import { FeeModule } from 'src/modules/fee/fee.module';
     OfferRequestModule,
     OfferPriceCalculatorModule,
     FeeModule,
-    SubscriptionManagerModule.forFeature({
-      imports: [FirebaseModule, OfferHubSpotModule],
-      subscriptions: [OfferHubSpotSubscription],
-    }),
   ],
   providers: [OfferHubSpotService],
   exports: [OfferHubSpotService],
