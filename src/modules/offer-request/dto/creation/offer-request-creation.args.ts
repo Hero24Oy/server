dbc80b7d265eb2184e0714c790f0a1d739101590
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2023-08-23 00:34:24 +0300

    Use OfferRequestCreationInput

--- src/modules/offer-request/dto/creation/offer-request-creation.args.ts
+++ src/modules/offer-request/dto/creation/offer-request-creation.args.ts
@@ -1,22 +1,8 @@
-import { ArgsType, Field, Float } from '@nestjs/graphql';
-import { OfferRequestDataInput } from './offer-request-data.input';
-import { OfferRequestSubscriptionInput } from './offer-request-subscription.input';
-import { MaybeType } from 'src/modules/common/common.types';
+import { ArgsType, Field } from '@nestjs/graphql';
+import { OfferRequestCreationInput } from './offer-request-creation.input';
 
 @ArgsType()
 export class OfferRequestCreationArgs {
-  @Field(() => OfferRequestDataInput)
-  data: OfferRequestDataInput;
-
-  @Field(() => OfferRequestSubscriptionInput, { nullable: true })
-  subscription?: MaybeType<OfferRequestSubscriptionInput>;
-
-  @Field(() => Float, { nullable: true })
-  customerVat?: MaybeType<number>;
-
-  @Field(() => Float, { nullable: true })
-  minimumDuration?: MaybeType<number>;
-
-  @Field(() => Float, { nullable: true })
-  serviceProviderVAT?: MaybeType<number>;
+  @Field(() => OfferRequestCreationInput)
+  input: OfferRequestCreationInput;
 }
