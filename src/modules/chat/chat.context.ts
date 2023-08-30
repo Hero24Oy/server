c4f707cc5d50ee606d29a8010d15d516733a19d5
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2023-08-30 17:28:29 +0300

    Use chat message loader

---
+++ src/modules/chat/chat.context.ts
@@ -0,0 +1,21 @@
+// chatMessageLoader: DataLoader<string, ChatMessageDto | null>;
+import DataLoader from 'dataloader';
+import { Injectable } from '@nestjs/common';
+
+import { AppGraphQLContext } from 'src/app.types';
+
+import { GraphQLContextProviderService } from '../graphql-context-manager/graphql-context-manager.interface';
+import { ChatMessageService } from './services/chat-message.service';
+
+@Injectable()
+export class ChatContext implements GraphQLContextProviderService {
+  constructor(private chatMessageService: ChatMessageService) {}
+
+  async createContext(): Promise<Partial<AppGraphQLContext>> {
+    return {
+      chatMessageLoader: new DataLoader((ids) =>
+        this.chatMessageService.getChatMessageByIds(ids),
+      ),
+    };
+  }
+}
