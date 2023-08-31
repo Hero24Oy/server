41df8bdd3ce29f3ea90e2c564712cdfec6c61323
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2023-08-31 16:59:38 +0300

    Fix input

--- src/modules/offer-request/dto/editing/offer-request-update-addresses.input.ts
+++ src/modules/offer-request/dto/editing/offer-request-update-addresses.input.ts
@@ -1,9 +1,10 @@
-import { InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
 import { OfferRequestIdInput } from 'src/modules/offer/dto/editing/offer-request-id.input';
 import { AddressesAnsweredInput } from '../address-answered/addresses-answered.input';
 
 @InputType()
 export class OfferRequestUpdateAddressesInput extends OfferRequestIdInput {
+  @Field(() => AddressesAnsweredInput)
   addresses: AddressesAnsweredInput;
 }
