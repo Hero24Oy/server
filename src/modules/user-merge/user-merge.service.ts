f8c5e4a0bb8eb8472b59c6d80d53d35cc5b225ae
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2023-09-23 02:35:20 +0300

    Fix user-merge service

--- src/modules/user-merge/user-merge.service.ts
+++ src/modules/user-merge/user-merge.service.ts
@@ -1,31 +1,28 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { PubSub } from 'graphql-subscriptions';
+import { Injectable } from '@nestjs/common';
 import { UserMergeDB } from 'hero24-types';
 
 import { Identity } from '../auth/auth.types';
 import { FirebaseDatabasePath } from '../firebase/firebase.constants';
 import { FirebaseService } from '../firebase/firebase.service';
-import { PUBSUB_PROVIDER } from '../graphql-pubsub/graphql-pubsub.constants';
+import { FirebaseTableReference } from '../firebase/firebase.types';
 
 import { UserMergeDto } from './dto/user-merge/user-merge.dto';
 import { UserMergeInput } from './dto/user-merge/user-merge.input';
 
 @Injectable()
 export class UserMergeService {
-  constructor(
-    private firebaseService: FirebaseService,
-    @Inject(PUBSUB_PROVIDER) private pubSub: PubSub,
-  ) {}
+  private userMergeTableRef: FirebaseTableReference<UserMergeDB>;
+
+  constructor(firebaseService: FirebaseService) {
+    const database = firebaseService.getDefaultApp().database();
+
+    this.userMergeTableRef = database.ref(FirebaseDatabasePath.USER_MERGES);
+  }
 
   async getUserMergeByUserId(userId: string): Promise<UserMergeDto | null> {
-    const database = this.firebaseService.getDefaultApp().database();
+    const userMergeSnapshot = await this.userMergeTableRef.child(userId).get();
 
-    const userMergeSnapshot = await database
-      .ref(FirebaseDatabasePath.USER_MERGES)
-      .child(userId)
-      .once('value');
-
-    const userMerge: UserMergeDB | null = userMergeSnapshot.val();
+    const userMerge = userMergeSnapshot.val();
 
     return userMerge && UserMergeDto.adapter.toExternal(userMerge);
   }
@@ -44,17 +41,12 @@ export class UserMergeService {
     userMergeInput: UserMergeInput,
     identity: Identity,
   ): Promise<UserMergeDto> {
-    const database = this.firebaseService.getDefaultApp().database();
-
     const newUserMerge = UserMergeInput.adapter.toInternal({
       ...userMergeInput,
       userId: identity.id,
     });
 
-    await database
-      .ref(FirebaseDatabasePath.USER_MERGES)
-      .child(identity.id)
-      .set(newUserMerge);
+    await this.userMergeTableRef.child(identity.id).set(newUserMerge);
 
     return this.strictGetUserMergeByUserId(identity.id);
   }
