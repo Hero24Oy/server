75f40696e46e99b2c28b41b793ce96510616c78f
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2023-09-26 23:20:40 +0300

    Create buyer mirror

--- src/modules/buyer/buyer.module.ts
+++ src/modules/buyer/buyer.module.ts
@@ -4,8 +4,11 @@ import { FirebaseModule } from '../firebase/firebase.module';
 import { GraphQlContextManagerModule } from '../graphql-context-manager/graphql-context-manager.module';
 
 import { BuyerContext } from './buyer.context';
+import { BuyerMirror } from './buyer.mirror';
 import { BuyerResolver } from './buyer.resolver';
 import { BuyerService } from './buyer.service';
+
+import { SubscriptionManagerModule } from '$modules/subscription-manager/subscription-manager.module';
 
 @Module({
   imports: [
@@ -13,6 +16,10 @@ import { BuyerService } from './buyer.service';
     GraphQlContextManagerModule.forFeature({
       imports: [BuyerModule],
       contexts: [BuyerContext],
+    }),
+    SubscriptionManagerModule.forFeature({
+      imports: [FirebaseModule],
+      subscriptions: [BuyerMirror],
     }),
   ],
   providers: [BuyerResolver, BuyerService],
