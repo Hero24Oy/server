9e6b60e08cba216eff721d9b19ca9b9aa466375e
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-03 23:49:07 +0300

    Remove ArgsType for HeroPortfolioListInput.

--- src/modules/hero-portfolio/dto/hero-portfolio-list/hero-portfolio-list.input.ts
+++ src/modules/hero-portfolio/dto/hero-portfolio-list/hero-portfolio-list.input.ts
@@ -1,11 +1,10 @@
-import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
 import { HeroPortfolioListOrderInput } from './hero-portfolio-list-order.input';
 
 import { MaybeType } from '$modules/common/common.types';
 import { PaginationArgs } from '$modules/common/dto/pagination.args';
 
-@ArgsType()
 @InputType('HeroPortfolioListInput')
 export class HeroPortfolioListInput extends PaginationArgs {
   @Field(() => String)
