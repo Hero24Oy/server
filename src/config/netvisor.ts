03a5a26bf84521451b6dca3c2872adf976020e20
dmytroipatov <dmytro.ipatov@sysgears.com>
2023-10-11 16:25:11 +0300

    add support days separated by commas

--- src/config/netvisor.ts
+++ src/config/netvisor.ts
@@ -1,5 +1,13 @@
 import * as Joi from 'joi';
 
+let FETCH_DAY_MESSAGE = '';
+FETCH_DAY_MESSAGE += 'Fetch day must be: \n';
+FETCH_DAY_MESSAGE += '1. A number in the range 1-6 \n';
+FETCH_DAY_MESSAGE += '2. Range x-y (x = 1...7, y = 1...7, x < y) \n';
+FETCH_DAY_MESSAGE +=
+  '3. Numbers in the range from 1 to 6 separated by commas(1,3,5)\n';
+FETCH_DAY_MESSAGE += '4. * for all days';
+
 export const netvisorValidationSchema = Joi.object({
   NETVISOR_BASE_URL: Joi.string().required(),
   NETVISOR_ORG_ID: Joi.string().required(),
@@ -9,11 +17,10 @@ export const netvisorValidationSchema = Joi.object({
   NETVISOR_PARTNER_KEY: Joi.string().required(),
   NETVISOR_SENDER: Joi.string().required(),
   NETVISOR_FETCH_DAY: Joi.string()
-    .pattern(/^([1-7]|[1-7]-[1-6])|\*$/)
+    .pattern(/^([1-7]|[1-7]-[1-6])|([1-6],|[1-6]){1,6}|\*$/)
     .required()
     .messages({
-      'string.pattern.base':
-        'Fetch day must be a number in the range 1-6 or range x-y (x = 1...7, y = 1...7, x < y) or * for all days',
+      'string.pattern.base': FETCH_DAY_MESSAGE,
     }),
   NETVISOR_FETCH_HOURS: Joi.string()
     .pattern(/^([1-9]|1[0-9]|2[0-4])$/)
