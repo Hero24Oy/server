48715911369c8b94b83cbea273eaab007e799c64
Andrii Zinko <andrii.zinko@sysgears.com>
2023-10-12 20:08:13 +0300

    Replace firebase subscriptions from module to UserMergeSubscription class.

--- src/modules/user-merge/user-merge.module.ts
+++ src/modules/user-merge/user-merge.module.ts
@@ -1,59 +1,23 @@
-import { Inject, Module } from '@nestjs/common';
-import { PubSub } from 'graphql-subscriptions';
+import { Module } from '@nestjs/common';
 
-import { skipFirst } from '../common/common.utils';
 import { FirebaseModule } from '../firebase/firebase.module';
-import { subscribeOnFirebaseEvent } from '../firebase/firebase.utils';
-import { PUBSUB_PROVIDER } from '../graphql-pubsub/graphql-pubsub.constants';
 import { GraphQlPubsubModule } from '../graphql-pubsub/graphql-pubsub.module';
 
-import {
-  createUserMergeAddedEventHandler,
-  createUserMergeUpdatedEventHandler,
-} from './user-merge.event-handlers';
 import { UserMergeResolver } from './user-merge.resolver';
 import { UserMergeService } from './user-merge.service';
+import { UserMergeSubscription } from './user-merge.subscription';
+
+import { SubscriptionManagerModule } from '$modules/subscription-manager/subscription-manager.module';
 
 @Module({
-  imports: [FirebaseModule, GraphQlPubsubModule],
+  imports: [
+    FirebaseModule,
+    GraphQlPubsubModule,
+    SubscriptionManagerModule.forFeature({
+      imports: [FirebaseModule, GraphQlPubsubModule, UserMergeModule],
+      subscriptions: [UserMergeSubscription],
+    }),
+  ],
   providers: [UserMergeResolver, UserMergeService],
 })
-export class UserMergeModule {
-  static unsubscribes: Array<() => Promise<void> | void> = [];
-
-  constructor(
-    private readonly userMergeService: UserMergeService,
-    @Inject(PUBSUB_PROVIDER) private pubSub: PubSub,
-  ) {}
-
-  onApplicationBootstrap(): void {
-    this.subscribeOnUserMergeUpdates(this.pubSub);
-  }
-
-  subscribeOnUserMergeUpdates(pubsub: PubSub): void {
-    const { userMergeTableRef } = this.userMergeService;
-
-    UserMergeModule.unsubscribes = [
-      subscribeOnFirebaseEvent(
-        userMergeTableRef,
-        'child_changed',
-        createUserMergeUpdatedEventHandler(pubsub),
-      ),
-      subscribeOnFirebaseEvent(
-        // Firebase child added event calls on every exist item first, than on every creation event.
-        // So we should skip every exists items using limit to last 1 so as not to retrieve all items
-        userMergeTableRef.limitToLast(1),
-        'child_added',
-        skipFirst(createUserMergeAddedEventHandler(pubsub)),
-      ),
-    ];
-  }
-
-  async beforeApplicationShutdown(): Promise<void> {
-    await Promise.all(
-      UserMergeModule.unsubscribes.map((unsubscribe) => unsubscribe()),
-    );
-
-    UserMergeModule.unsubscribes = [];
-  }
-}
+export class UserMergeModule {}
---
+++ src/modules/user-merge/user-merge.subscription.ts
@@ -0,0 +1,63 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
+import { UserMergeDB } from 'hero24-types';
+
+import { skipFirst } from '../common/common.utils';
+import { FirebaseReference } from '../firebase/firebase.types';
+import { subscribeOnFirebaseEvent } from '../firebase/firebase.utils';
+import { PUBSUB_PROVIDER } from '../graphql-pubsub/graphql-pubsub.constants';
+import {
+  SubscriptionService,
+  Unsubscribe,
+} from '../subscription-manager/subscription-manager.types';
+
+import {
+  createUserMergeAddedEventHandler,
+  createUserMergeUpdatedEventHandler,
+} from './user-merge.event-handlers';
+import { UserMergeService } from './user-merge.service';
+
+@Injectable()
+export class UserMergeSubscription implements SubscriptionService {
+  constructor(
+    private readonly userMergeService: UserMergeService,
+    @Inject(PUBSUB_PROVIDER) private readonly pubSub: PubSub,
+  ) {}
+
+  public async subscribe(): Promise<Unsubscribe> {
+    const { userMergeTableRef } = this.userMergeService;
+
+    const unsubscribes = [
+      await this.subscribeOnUserMergeUpdates(userMergeTableRef, this.pubSub),
+      await this.subscribeOnUserMergeAdding(userMergeTableRef, this.pubSub),
+    ];
+
+    return async () => {
+      await Promise.all(unsubscribes.map((unsubscribe) => unsubscribe()));
+    };
+  }
+
+  private async subscribeOnUserMergeUpdates(
+    rootUserMergeRef: FirebaseReference<Record<string, UserMergeDB>>,
+    pubsub: PubSub,
+  ): Promise<() => void> {
+    return subscribeOnFirebaseEvent(
+      rootUserMergeRef,
+      'child_changed',
+      createUserMergeUpdatedEventHandler(pubsub),
+    );
+  }
+
+  private async subscribeOnUserMergeAdding(
+    rootUserMergeRef: FirebaseReference<Record<string, UserMergeDB>>,
+    pubsub: PubSub,
+  ): Promise<() => void> {
+    return subscribeOnFirebaseEvent(
+      // Firebase child added event calls on every exist item first, than on every creation event.
+      // So we should skip every exists items using limit to last 1 so as not to retrieve all items
+      rootUserMergeRef.limitToLast(1),
+      'child_added',
+      skipFirst(createUserMergeAddedEventHandler(pubsub)),
+    );
+  }
+}
