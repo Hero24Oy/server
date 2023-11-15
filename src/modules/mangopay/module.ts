100923fb6098e4e85582a0087de59fa8d4b99d6d
dmytroipatov <dmytro.ipatov@sysgears.com>
2023-11-15 17:15:41 +0200

    setup mangopay module

--- src/modules/mangopay/module.ts
+++ src/modules/mangopay/module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { MangopayService } from './service';
+
 @Module({
   imports: [],
-  providers: [],
-  exports: [],
+  providers: [MangopayService],
+  exports: [MangopayService],
 })
 export class MangopayModule {}
---
+++ src/modules/mangopay/service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import MangopayApi from 'mangopay2-nodejs-sdk';
+
+import { ConfigType } from '$config';
+import { Config } from '$decorator';
+
+@Injectable()
+export class MangopayService {
+  private readonly api: MangopayApi;
+
+  constructor(@Config() config: ConfigType) {
+    this.api = new MangopayApi(config.mangopay);
+  }
+}
