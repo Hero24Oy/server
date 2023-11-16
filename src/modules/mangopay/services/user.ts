8e1848592a2068439655069458d3d1aeb57b3c3d
dmytroipatov <dmytro.ipatov@sysgears.com>
2023-11-16 17:15:52 +0200

    add user update

--- src/modules/mangopay/services/user.ts
+++ src/modules/mangopay/services/user.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { user as MangoPayUser } from 'mangopay2-nodejs-sdk';
 
-import { PersonType } from '../enums';
+import { MangopayPersonType } from '../enums';
 
 import { MangopayInstanceService } from './instance';
 
@@ -11,23 +11,43 @@ import { MangopayParameters } from '$modules/mangopay/types';
 export class MangopayUserService {
   constructor(private readonly api: MangopayInstanceService) {}
 
-  async createNaturalUser(
+  async createSelfEmployedUser(
     user: MangoPayUser.CreateUserNaturalData,
   ): Promise<MangoPayUser.UserNaturalData> {
     return this.api.Users.create({
       ...user,
       TermsAndConditionsAccepted: true,
-      PersonType: PersonType.NATURAL,
+      PersonType: MangopayPersonType.SELF_EMPLOYED,
     });
   }
 
-  async createLegalUser(
+  async createCompanyUser(
     user: MangoPayUser.CreateUserLegalData,
   ): Promise<MangoPayUser.UserLegalData> {
     return this.api.Users.create({
       ...user,
       TermsAndConditionsAccepted: true,
-      PersonType: PersonType.LEGAL,
+      PersonType: MangopayPersonType.COMPANY,
+    });
+  }
+
+  async updateSelfEmployedUser(
+    user: MangoPayUser.UpdateUserNaturalData,
+  ): Promise<MangoPayUser.UserNaturalData> {
+    return this.api.Users.update({
+      ...user,
+      TermsAndConditionsAccepted: true,
+      PersonType: MangopayPersonType.SELF_EMPLOYED,
+    });
+  }
+
+  async updateCompanyUser(
+    user: MangoPayUser.UpdateUserLegalData,
+  ): Promise<MangoPayUser.UserLegalData> {
+    return this.api.Users.update({
+      ...user,
+      TermsAndConditionsAccepted: true,
+      PersonType: MangopayPersonType.COMPANY,
     });
   }
 
