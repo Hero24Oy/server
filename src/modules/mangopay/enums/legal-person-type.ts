40c59ff50d4622e599fae816500c5cd0d1728705
dmytroipatov <dmytro.ipatov@sysgears.com>
2023-11-20 15:56:59 +0200

    delete old file

--- src/modules/mangopay/enums/legal-person-type.ts
+++
@@ -1,8 +0,0 @@
-// Subtypes Professional(Legal) user
-// More: https://mangopay.com/docs/concepts/users/verification/requirements-legal
-export enum MangopayLegalPersonType {
-  BUSINESS = 'BUSINESS',
-  PARTNERSHIP = 'PARTNERSHIP',
-  ORGANIZATION = 'ORGANIZATION',
-  SOLETRADER = 'SOLETRADER',
-}
