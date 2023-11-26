77fb556c71a19636505a15b69a45fcbc1230fcc8
Andrii Zinko <andrii.zinko@sysgears.com>
2023-11-26 18:22:34 +0200

    Minor fixes.

--- src/modules/file/graphql/objects/data-without-storage-path.ts
+++ src/modules/file/graphql/objects/data-without-storage-path.ts
@@ -2,6 +2,7 @@ import { InputType, ObjectType, OmitType } from '@nestjs/graphql';
 
 import { FileDataObject } from './data';
 
+@InputType()
 @ObjectType()
 export class FileDataWithoutStoragePathObject extends OmitType(
   FileDataObject,
