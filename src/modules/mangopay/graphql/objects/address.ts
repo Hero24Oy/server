a2811a635cd80716128914072f01dc3750c89dd1
dmytroipatov <dmytro.ipatov@sysgears.com>
2023-11-28 14:08:18 +0200

    update address object

--- src/modules/mangopay/graphql/objects/address.ts
+++ src/modules/mangopay/graphql/objects/address.ts
@@ -1,7 +1,8 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CountryISO } from 'mangopay2-nodejs-sdk';
 
-@InputType()
+@InputType('MangopayAddressInput')
+@ObjectType()
 export class MangopayAddressObject {
   @Field(() => String)
   addressLine: string;
