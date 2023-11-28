e99df2cae8f438466d5d5f6f0b33901460cc1b8d
dmytroipatov <dmytro.ipatov@sysgears.com>
2023-11-28 13:56:41 +0200

    update export name

--- src/modules/mangopay/graphql/resolvers/index.ts
+++ src/modules/mangopay/graphql/resolvers/index.ts
@@ -2,4 +2,4 @@ export * from './card-registration';
 export * from './cards';
 export * from './deactivate-card';
 export * from './update-card-registration';
-export * from './create-direct-card-pay-in';
+export * from './make-direct-card-pay-in';
