7060d0f9cfebe84e651a58be1c159e5480e1f06b
mairh <ujjwal.mairh@gmail.com>
2024-09-01 14:08:33 +0300

    change vat to 25.5

--- src/modules/fee/fee.constants.ts
+++ src/modules/fee/fee.constants.ts
@@ -1,5 +1,5 @@
-export const FEE_SERVICE_PROVIDER_VAT = 24;
-export const FEE_CUSTOMER_VAT = 24;
+export const FEE_SERVICE_PROVIDER_VAT = 25.5;
+export const FEE_CUSTOMER_VAT = 25.5;
 
 export const FEE_UPDATED_SUBSCRIPTION = 'feeUpdated';
 export const FEE_CREATED_SUBSCRIPTION = 'feeCreated';
